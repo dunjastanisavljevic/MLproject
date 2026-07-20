f7db4c6c4051bef4cbbd264f79341740a07ac597
dunjastanisavljevic <tvoj_github_dunjastanisavljevicc@gmail.com>
2026-07-20 22:51:06 +0200

    Add libraries and load heart disease dataset

--- python_project.py
+++ python_project.py
@@ -1,1 +1,30 @@
 print("Machine Learning projekat inicijalizovan.")
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    ConfusionMatrixDisplay
+)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+df = pd.read_csv(
+    "data/HeartDiseaseRiskPredictionDataset/heart_disease_prediction.csv"
+)
+
+print("First five rows of the dataset:")
+print(df.head())
